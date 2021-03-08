e92da1fcb30a6ea750c181bde06be672f25f0536
Philipp Moritzer <philipp.moritzer@yahoo.de>
2021-03-08 05:22:54 -0800

    really not damaged at all anymore

--- web-app/index.js
+++ web-app/index.js
@@ -5,10 +5,14 @@ const { Kafka } = require('kafkajs')
 const mysqlx = require('@mysql/xdevapi');
 const MemcachePlus = require('memcache-plus');
 const express = require('express')
+const path = require('path')
 
 const app = express()
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+
 const cacheTimeSecs = 15;
-const numberOfFoods = 12;
 
 const {generateDataset} = require('./data');
 
@@ -151,50 +155,6 @@ async function sendStudentMessage(data) {
 
 
 // -------------------------------------------------------
-// HTML helper to send a response to the client
-// -------------------------------------------------------
-
-function sendResponse(res, html, cachedResult) {
-	res.send(`<!DOCTYPE html>
-		<html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<title>Big Data Use-Case Demo</title>
-			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.1/mini-default.min.css">
-			<script>
-				function generateRandomSurveys() {
-					const maxRepetitions = Math.floor(Math.random() * 200)
-					document.getElementById("out").innerText = "Generating " + maxRepetitions + " random surveys, see console output"
-					for(var i = 0; i < maxRepetitions; ++i) {
-						console.log("Generating" +  maxRepetitions +  " Survey Datasets")
-						fetch("/survey/", {cache: 'no-cache'})
-					}
-				}
-			</script>
-		</head>
-		<body>
-			<h1>Big Data Use Case Demo</h1>
-			<h1>Hello World!</h1>
-			<p>
-				<a href="javascript: generateRandomSurveys();">Randomly fetch some missions</a>
-				<span id="out"></span>
-			</p>
-			${html}
-			<hr>
-			<h2>Information about the generated page</h4>
-			<ul>
-				<li>Server: ${os.hostname()}</li>
-				<li>Date: ${new Date()}</li>
-				<li>Using ${memcachedServers.length} memcached Servers: ${memcachedServers}</li>
-				<li>Cached result: ${cachedResult}</li>
-			</ul>
-		</body>
-	</html>
-	`)
-}
-
-// -------------------------------------------------------
 // Start page
 // -------------------------------------------------------
 
@@ -211,7 +171,8 @@ async function getFoods() {
 		let executeResult = await executeQuery("SELECT id, name FROM food", [])
 		let data = executeResult.fetchAll()
 		if (data) {
-			let result = data.map(row => row);
+			let result = data.map(row => ({id: row[0], name: row[1]}));
+			
 			console.log(`Got result=${result}, storing in cache`)
 			if (memcached)
 				await memcached.set(key, result, cacheTimeSecs);
@@ -233,49 +194,47 @@ async function getFoods() {
 // 		.map(row => ({ mission: row[0], count: row[1] }))
 // }
 
-// Get popular missions (from db only)
+// Get popular cuisines (from db only)
 async function getSmartCuisine(maxCount) {
 	const query = "SELECT cuisine, avg_gpa, count FROM smart_cuisine ORDER BY avg_gpa DESC LIMIT ?"
 	return (await executeQuery(query, [maxCount]))
 		.fetchAll()
-		.map(row => { console.log("#######: " + row); return ({ cuisine: row[0], avg_gpa: row[1], count: row[2] }); })
+		.map(row => { console.log("#######: " + row); return ({ cuisine: row[0], avg_gpa: Number(row[1].toFixed(2)), count: row[2] }); })
+}
+
+// Get popular lunches (from db only)
+async function getSmartLunch(maxCount) {
+	const query = "SELECT lunch, avg_gpa, count FROM smart_lunch ORDER BY avg_gpa DESC LIMIT ?"
+	return (await executeQuery(query, [maxCount]))
+		.fetchAll()
+		.map(row => { console.log("#######: " + row); return ({ lunch: row[0], avg_gpa: Number(row[1].toFixed(2)), count: row[2] }); })
+}
+
+// Get popular breakfasts (from db only)
+async function getSmartBreakfast(maxCount) {
+	const query = "SELECT breakfast, avg_gpa, count FROM smart_breakfast ORDER BY avg_gpa DESC LIMIT ?"
+	return (await executeQuery(query, [maxCount]))
+		.fetchAll()
+		.map(row => { console.log("#######: " + row); return ({ breakfast: row[0], avg_gpa: Number(row[1].toFixed(2)), count: row[2] }); })
 }
 
 // Return HTML for start page
 app.get("/", (req, res) => {
 	const topX = 10;
-	Promise.all([getFoods(), getSmartCuisine(topX)]).then(values => {  //, getPopular(topX)
-		const food = values[0]
-		//const popular = values[1]
-		const smartCuisine = values[1]
+	Promise.all([getFoods(), getSmartCuisine(topX), getSmartLunch(topX), getSmartBreakfast(topX)]).then(values => {  //, getPopular(topX)
+		const foods = values[0];
+		const smartCuisine = values[1];
+		const smartLunch = values[2];
+		const smartBreakfast = values[3];
 
-		
-		console.log('values: ', values);
-		console.log('values 0: ', values[0]);
-		console.log('values 0: ', values[1]);
-		const foodHtml = food.result
-		 	.map(m => `<a href='food/${m[0]}'>${m[1]}</a>`)
-		 	.join(", ")
-
-		// const popularHtml = popular
-		// 	.map(pop => `<li> <a href='missions/${pop.mission}'>${pop.mission}</a> (${pop.count} views) </li>`)
-		// 	.join("\n")
-		//			<ol style="margin-left: 2em;"> ${popularHtml} </ol> 
-		//const cuisineHtml ="<h1>mjam</h1>"
-		 const cuisineHtml = smartCuisine
-		 	.map(pop => `<li> ${pop.cuisine}(${pop.avg_gpa} gpa) </li>`)
-		 	.join("\n")
 					
+		const parameters = {
+			foods: foods.result, smartCuisine, smartBreakfast, smartLunch, cachedResult: foods.cached, topX,
+			hostname: os.hostname(), date: new Date(), memcachedServers, 
+		}
+		res.render(path.join(__dirname, 'public/index.html'), 
+		parameters);
 
-		const html = `
-			<h1>Top ${topX} Foods</h1>		
-			<p>
-			<ol style="margin-left: 2em;"> ${cuisineHtml} </ol> 
-			</p>
-			<h1>All Foods</h1>
-			<p> ${foodHtml} </p>
-		`
-		sendResponse(res, html, food.cached)
 	})
 })
 
@@ -316,24 +275,26 @@ app.get("/survey", (req, res) => {
 	sendStudentMessage(student).then(() => console.log("Sent Student to kafka"))
 		.catch(e => console.log("Student Error sending to kafka", e))
 
-	sendResponse(res, `<h1>Hello From survey</h1><p></p>`)
-
-	// Send reply to browser
-	// getMission(mission).then(data => {
-	// 	sendResponse(res, `<h1>${data.mission}</h1><p>${data.heading}</p>` +
-	// 		data.description.split("\n").map(p => `<p>${p}</p>`).join("\n"),
-	// 		data.cached
-	// 	)
-	// }).catch(err => {
-	// 	sendResponse(res, `<h1>Error</h1><p>${err}</p>`, false)
-	// })
+	res.send("Survey done");
+	
 });
 
 app.get("/food/:foodId", (req, res) => {
 	let foodId = req.params["foodId"];
 	console.log(foodId);
 	getFood(foodId).then(data => {
-		sendResponse(res, `<h1>hello from ${data.name}</h1><span>${data.description}</span>`, data.cached)
+
+		
+		//foods: foods.result, smartCuisine, smartBreakfast, smartLunch, cachedResult: foods.cached, topX,
+		//	hostname: os.hostname(), date: new Date(), memcachedServers, 
+		const parameters = {
+			food: data, cachedResult: data.cached, hostname: os.hostname(), date: new Date(), memcachedServers
+
+		}
+		res.render(path.join(__dirname, 'public/detail.html'), 
+		parameters);
+
+		//sendResponse(res, `<h1>hello from ${data.name}</h1><span>${data.description}</span>`, data.cached)
 	}).catch(err => {
 		console.error(err)
 	})
@@ -345,6 +306,9 @@ app.get("/food/:foodId", (req, res) => {
 // Main method
 // -------------------------------------------------------
 
+app.use(express.static('/public'));
+
 app.listen(options.port, function () {
 	console.log("Node app is running at http://localhost:" + options.port)
+	
 });
